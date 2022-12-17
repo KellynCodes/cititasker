3e87e41c69dcabef4363191509cc99d4197d64f0
prokelly <kellyrobot247@gmail.com>
2022-12-17 11:00:36 +0100

    Added index and 4 orther pages

---
+++ pages/emailVerificationExpired.jsx
@@ -0,0 +1,49 @@
+import React from "react";
+import Navbar from "../components/Navbar";
+import Head from "next/Head";
+import styles from "../styles/EmailVerificationExpired.module.css";
+
+const emailVerificationExpired = () => {
+  return (
+    <>
+      <Head>
+        <title>CitiTasker | password Recover success</title>
+        <meta name="description" content="CitiTasker" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Navbar />
+      <div className={styles.emailVerificationExpiredContainer}>
+        <div className={styles.emailVerificationExpired}>
+          <img
+            src="emailVerificationExpired.jpg"
+            alt="emailMessage Box Image"
+            className={styles.emailVerificationExpiredImage}
+          />
+
+          <div className={styles.MessagePart}>
+            <div className={styles.MessagePartTop}>
+              <h2>Email verification link has expired</h2>
+              <p>
+                Looks like the verification link has expired. Not to worry, we
+                can send the link again
+              </p>
+            </div>
+            <div className={styles.MessagePartBottom}>
+              <a href="#" className={styles.ResendLinkButton}>
+                Resend verification link
+              </a>
+              <p>
+                <a href="#" className={styles.clickHere}>
+                  Click here
+                </a>
+                if you did not receive the link in your inbox or if you would
+                like to change the email address you signed up with
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+export default emailVerificationExpired;
